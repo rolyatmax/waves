654a543dd881ba8d94405f64c3203720ee4624e0
Taylor Baldwin <taylorbaldwin@gmail.com>
2017-01-30 21:59:19 -0500

    ready to go!

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import sortBy from 'lodash/sortBy'
 import bspline from 'b-spline'
 import tinycolor from 'tinycolor2'
 import InfoBox from './lib/info-box'
+import { GUI } from 'dat-gui'
 import colorPalettes from './lib/color-palettes.json'
 
 const seed = Math.random()
@@ -14,29 +15,32 @@ const info = new InfoBox(document.querySelector('.info'))
 setTimeout(() => info.show(), 5000)
 
 const settings = {
-  lineCount: 10,
+  lineCount: 15,
   pointCount: 50,
   padding: 40,
   dotSize: 1,
-  colors: colorPalettes[rand() * colorPalettes.length | 0],
-  dotAlpha: 0.5,
-  lineAlpha: 0.8,
-  magnitude: 500,
-  splineDegree: 25,
-  speed: 1
+  colors: rand() * colorPalettes.length | 0,
+  dotAlpha: 0.1,
+  lineAlpha: 0.7,
+  magnitude: 120,
+  splineDegree: 0.25,
+  speed: 8,
+  darkTheme: false
 }
 
 const container = document.querySelector('.container')
-container.style.backgroundColor = '#333'
 
-Sketch.create({
+const waves = Sketch.create({
   container: container,
 
   setup () {
-    this.globalCompositeOperation = 'lighten'
+    const backgroundColor = settings.darkTheme ? '#333' : '#fbfbfb'
+    container.style.backgroundColor = backgroundColor
+    const blending = settings.darkTheme ? 'lighten' : 'darken'
+    this.globalCompositeOperation = blending
     const y = this.height / 2
     this.lines = newArray(settings.lineCount).map((line, i) => ({
-      color: settings.colors[i % settings.colors.length],
+      color: i,
       points: sortBy(newArray(settings.pointCount).map(() => ({
         position: [rand() * (this.width - settings.padding * 2) + settings.padding, y],
         speed: rand() * 2 - 1
@@ -61,23 +65,44 @@ Sketch.create({
   },
 
   draw () {
+    const colors = colorPalettes[settings.colors]
     this.lines.forEach(line => {
       const controlPoints = line.points.map(p => p.position)
-      const circleColor = tinycolor(line.color).setAlpha(settings.dotAlpha).toRgbString()
+      const color = colors[line.color % colors.length]
+      const circleColor = tinycolor(color).setAlpha(settings.dotAlpha).toRgbString()
       drawCircles(this, controlPoints, circleColor)
-      const points = calculateSplinePoints(controlPoints, settings.splineDegree)
-      const lineColor = tinycolor(line.color).setAlpha(settings.lineAlpha).toRgbString()
+      const degree = Math.max(1, settings.splineDegree * settings.pointCount | 0)
+      const points = calculateSplinePoints(controlPoints, degree)
+      const lineColor = tinycolor(color).setAlpha(settings.lineAlpha).toRgbString()
       drawLine(this, points, lineColor)
     })
   }
 })
 
+const restart = waves.setup.bind(waves)
+const newColor = () => {
+  settings.colors = rand() * colorPalettes.length | 0
+}
+const gui = new GUI()
+gui.add(settings, 'lineCount', 1, 100).step(1).onFinishChange(restart)
+gui.add(settings, 'pointCount', 2, 100).step(1).onFinishChange(restart)
+gui.add(settings, 'dotSize', 1, 100).step(1)
+gui.add(settings, 'dotAlpha', 0, 1).step(0.05)
+gui.add(settings, 'lineAlpha', 0, 1).step(0.05)
+gui.add(settings, 'magnitude', 1, 500).step(5)
+gui.add(settings, 'splineDegree', 0.0, 0.95).step(0.05)
+gui.add(settings, 'speed', 1, 40).step(1)
+gui.add(settings, 'darkTheme').onFinishChange(restart)
+gui.add({ newColor }, 'newColor') // todo: linearly interpolate to new colors
+
 function calculateSplinePoints (controls, degree) {
   const points = []
-  let progress = 0
-  while (progress < 1) {
+  const granularity = 100
+  let i = 0
+  while (i < granularity) {
+    const progress = i / granularity
     points.push(bspline(progress, degree, controls))
-    progress += 0.01
+    i += 1
   }
   return points
 }
@@ -88,7 +113,6 @@ function drawLine (ctx, points, color) {
   ctx.moveTo(points[0][0], points[0][1])
   points.slice(1).forEach(point => {
     ctx.lineTo(point[0], point[1])
-    // drawCircle(ctx, point[0], point[1], settings.dotSize, color)
   })
   ctx.stroke()
 }
